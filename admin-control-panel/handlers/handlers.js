5b14b0b7bcfb35cbf0f9531b4221425519192aa6
Tahir-Ahmad-Wani <wanitahir59@gmail.com>
2024-11-25 02:08:05 +0530

    added features

--- admin-control-panel/handlers/handlers.js
+++ admin-control-panel/handlers/handlers.js
@@ -1,8 +1,7 @@
 //=================================================================================================
 //=========================== Show/Hide Elements using event listener =============================
 //=================================================================================================
-let previousClickedTabButton = ''
-let previousDisplayedTabSpace = ''
+
 // Add event listeners for elements to display them using custom function
 function toggTabVisibilityOnClick(srcElement, destElement){ // source s the clicked element and dest is the eleent that reacts on click
     document.getElementById(srcElement).addEventListener('click', toggleVisibility(srcElement,destElement))
@@ -10,25 +9,37 @@ function toggTabVisibilityOnClick(srcElement, destElement){ // source s the clic
 function toggleVisibility(srcElement,destElement){
     return ()=>{ //use of closure
         try{
-            makeInVisible(previousDisplayedTabSpace)
+            makeInVisible([previousDisplayedTabSpace])
             changeBackgroundColor(previousClickedTabButton, '#33b5e5')
         }
         catch(error){
             console.log('This error was handled by Tahir Ahmad Wani: ', error)
         }
         changeBackgroundColor(srcElement, '#0099cc')
-        document.getElementById(destElement).style.display = 'block'
+        makeVisible([destElement], 'block')
         previousClickedTabButton = srcElement
         previousDisplayedTabSpace = destElement
     }
 }
-function makeInVisible(destElement){
-        document.getElementById(destElement).style.display = 'none'
+function makeVisible(elementList, displayType){
+    for(i = 0; i < elementList.length; i++){ // an array of arguments must be passed
+        document.getElementById(elementList[i]).style.display = displayType // displayType takes values 'flex', 'block' etc
+    }
+    
+}
+function makeInVisible(elementList){ // an array of arguments must be passed
+    for(i = 0; i < elementList.length; i++){
+        document.getElementById(elementList[i]).style.display = 'none' 
+    }
 }
 //=================================== Function to change color =====================================
 
 function changeBackgroundColor(element, color){
     document.getElementById(element).style.backgroundColor = color
+}
+//============================== event listener and function binder  ==============================
+function addCustomEventListener(element ,event, callBack){ // this is custom eventlistener function that takes three arguments "element", "event", and "function to be called" as callback
+    document.getElementById(element).addEventListener(event, callBack(element)) 
 }
 
 
--- admin-control-panel/script.js
+++ admin-control-panel/script.js
@@ -1,2 +1,28 @@
-
-
+//================= Tab Custom Event Listeners ===========================
+let previousClickedTabButton = 'tab-button-1'
+let previousDisplayedTabSpace = ''
+// toggTabVisibilityOnClick is a function defined in handlers : Turns on the corresponding tab space where content is displayed
+// to keep first tab 'visible' by default toggleVisibility function is being called when the javascript will be loaded
+let toggle = toggleVisibility(previousClickedTabButton, 'tab-space-1') //this function cannot be called directly because we have implemented closures so this returns a reference to the callack which is stored into a variable and then called
+toggle() //this is the reference of toggleVisisbility function
+toggTabVisibilityOnClick('tab-button-1', 'tab-space-1')  // means clicking on tab-button-1 will make tab-space-1 visible
+toggTabVisibilityOnClick('tab-button-2', 'tab-space-2')
+toggTabVisibilityOnClick('tab-button-3', 'tab-space-3')
+toggTabVisibilityOnClick('tab-button-4', 'tab-space-4')
+toggTabVisibilityOnClick('tab-button-5', 'tab-space-5')
+toggTabVisibilityOnClick('tab-button-6', 'tab-space-6')
+toggTabVisibilityOnClick('tab-button-7', 'tab-space-7')
+//======================================================================= 
+let previousClickedPaymentReceiveDonationButton = 'receive-payment-options-donation-space'
+addCustomEventListener('payment-receive-donation-button', 'click', (element) => {
+return (element) => {
+  makeInVisible([previousClickedPaymentReceiveDonationButton]);
+  makeVisible(['receive-payment-options-donation-space'], 'flex');
+  previousClickedPaymentReceiveDonationButton = element
+}
+}) // this is custom eventlistener function that takes three arguments "element", "event", and "function to be called" as callback
+// ==========================================================================
+//hide receive paymement dialog box when clicked on cross
+// prototype "addCustomEventListener(String elementName, String eventType, callback)"
+addCustomEventListener('dialog-box-cross-btn', 'click', () => { return () => {makeInVisible(['receive-payments-dialog-box','dialog-boxes-container'])}})
+addCustomEventListener('receive-payment-button', 'click', () => { return () => {makeVisible(['receive-payments-dialog-box'],'block');makeVisible(['dialog-boxes-container'],'flex')}})
