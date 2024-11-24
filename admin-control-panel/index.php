51c26969e9fe8594a845f2122700c30ef783912e
Tahir-Ahmad-Wani <wanitahir59@gmail.com>
2024-11-25 02:25:19 +0530

    corrected meta tag in admin control panel

--- admin-control-panel/index.php
+++ admin-control-panel/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta name="viewport" content="width=device-width, initial-scale=2.0">
+    <meta name="viewport" content="width=device-width", initial-scale="1.0">
     <style>
       body{
         /* background: linear-gradient(135deg, #a0a1bd, #040406); */
